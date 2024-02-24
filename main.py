353090839e45605130f818f994b50c0235db1b9f
PLidiia <151846713+PLidiia@users.noreply.github.com>
2024-02-24 20:47:50 +0700

    Реклама с картинкой 2.0

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Миссия Колонизация Марса'
+    return 'Миссия Колонизация Марс'
 
 
-@app.route('/index')
-def index():
-    return 'И на Марсе будут яблони цвести!'
-
-
-@app.route('/promotion')
-def do_promo():
-    with open('design.html', 'r', encoding='utf-8') as design_file:
-        return design_file.read()
-
-
-@app.route('/image_mars')
+@app.route('/promotion_image')
 def do_image_mars():
-    with open('image_mars_design.html', 'r', encoding='utf-8') as design_file:
-        return design_file.read()
-
-
-@app.route('/astronaut_selection')
-def form_search_astronaut():
-    with open('astronaut_cosmos_form.html', 'r', encoding='utf-8') as design_file:
-        return design_file.read()
+    return f'''<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Колонизация</title>
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
+    <link rel="stylesheet" href="static/css/style.css">
+</head>
+<body>
+<h1>Жди нас, Марс!</h1>
+<img src="{url_for('static', filename='img/Mars.png')}" alt="Фото Марса">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+<div class="alert alert-dark" role="alert">
+  Человечество вырастает из детства.
+</div>
+<div class="alert alert-success" role="alert">
+  Человечеству мала одна планета.
+</div>
+<div class="alert alert-secondary" role="alert">
+  Мы сделаем обитаемыми безжизненные пока планеты.
+</div>
+<div class="alert alert-warning" role="alert">
+  И начнем с Марса!
+</div>
+<div class="alert alert-danger" role="alert">
+ Присоединяйся!
+</div>
+</body>
+</html>'''
 
 
 if __name__ == '__main__':
